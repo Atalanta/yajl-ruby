b62fa887a74cd3545d087478c620246d66983ac2
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2012-03-23 10:49:55 +0000

    Make extconf.rb aware of Solaris Studio compiler alternive options.

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -1,7 +1,27 @@
 require 'mkmf'
 require 'rbconfig'
 
-$CFLAGS << ' -Wall -funroll-loops'
-$CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
+def not_gcc?
+  (RbConfig::CONFIG['GCC'] != 'yes') and not (RbConfig::CONFIG['CC'].include?('gcc'))
+end
+
+def solaris?
+  RbConfig::CONFIG['arch'].include?("sparc") or RbConfig::CONFIG['arch'].include?("solaris")
+end
+
+def gcc_override?
+  ENV.key?("USE_GCC")
+end
+
+def use_solaris_studio_cflags?
+  solaris? and not_gcc? and not gcc_override?
+end
+
+if use_solaris_studio_cflags?
+  $CFLAGS << ' -xunroll=1' # Solaris studio requires a default; 1 means don't unroll loops.
+else
+  $CFLAGS << ' -Wall -funroll-loops'
+  $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
+end
 
 create_makefile('yajl/yajl')
